790a02753efec95af43920bae14187c5c4b1394a
kason <718087538@qq.com>
2020-06-17 23:23:54 +0800

    动态获取通知

--- pages/index/event.js
+++ pages/index/event.js
@@ -6,45 +6,26 @@ Page({
    */
   data: {
     eventList: [{
-        title: "今天的发货单，展示出来给大家看，如果展示出来给大家看，如果展示出来给大家看，如果",
-        srcList: [{
-            src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-          },
-          {
-            src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-          },
-          {
-            src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-          },
-        ]
-      },
-      {
-        title: "今天的发货单，展示出来给大家看，如果展示出来给大家看，如果展示出来给大家看，如果",
-        srcList: [{
-          src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-        }, ]
-      },
-      {
-        title: "今天的发货单，展示出来给大家看，如果展示出来给大家看，如果展示出来给大家看，如果",
-        srcList: [{
-            src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-          },
-          {
-            src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-          },
-          {
-            src: "http://img5.imgtn.bdimg.com/it/u=1040739115,3225906616&fm=26&gp=0.jpg"
-          },
-        ]
-      },
-    ]
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;//把this对象复制到临时变量that
+    const wxreq = wx.request({
+      url: 'http://api.orderour.com/api/wxClient/notice',
+      success: function (res){
+        console.log(res.data);
+        // this.userData = res.data; //无效不能实时的渲染到页面
+        that.setData({ eventList: res.data.data });//和页面进行绑定可以动态的渲染到页面
+      },
+      fail: function (res){
+        console.log(res.data);
+        this.userData = "数据获取失败";
+      }
+    })
   },
 
   /**
